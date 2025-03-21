9e149d06a9b68892e2ca73de7bedabbdaf512f6c
grangerb16 <grangerb@gmail.com>
2025-03-20 22:08:55 -0700

    print the report

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ def get_book_text(path):
     return contents
 
 from stats import count_words
-
 from stats import count_chars
+from stats import sorted_dict_list
 
 def main():
     text = get_book_text("./books/frankenstein.txt")
-    print(f'{count_words(text)} words found in the document')
-    print(count_chars(text))
+    char_count_dict = count_chars(text)
+    sorted_list = sorted_dict_list(char_count_dict)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(text)} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_list:
+        if dict["char"].isalpha():
+            print(f'{dict["char"]}: {dict["count"]}')
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,14 @@ def count_chars(text):
     for char in list(text.lower()):
         bow[char] = bow.get(char, 0) + 1
     return bow
+
+def sorted_dict_list(dict):
+    sorted_list = []
+
+    def sort_on(dict):
+        return dict["count"]
+
+    for key in dict:
+        sorted_list.append({"char": key, "count": dict[key]})
+        sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list  
