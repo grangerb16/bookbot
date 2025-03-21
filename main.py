112687a34d515cd58dfede8795caccd51e002647
grangerb16 <grangerb@gmail.com>
2025-03-20 22:19:01 -0700

    arguments and wrap-up

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_book_text(path):
     contents = ""
     with open(path) as file:
@@ -9,11 +11,15 @@ from stats import count_chars
 from stats import sorted_dict_list
 
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(f"./{sys.argv[1]}")
     char_count_dict = count_chars(text)
     sorted_list = sorted_dict_list(char_count_dict)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words(text)} total words")
     print("--------- Character Count -------")
